b48d398b207d0438e9e922fcee24c628b4910857
Stephen Kang <jkkang@sfu.ca>
2017-12-03 18:36:21 -0500

    Added proof of work check

--- go-chain.go
+++ go-chain.go
@@ -21,6 +21,7 @@ type block struct {
 	data			string
 	previous_hash	[]byte
 	hash 			[]byte
+	nonce			uint64
 }
 
 type blockchain struct {
@@ -48,6 +49,7 @@ func (bl *block) get_hash() []byte {
 	buffer.WriteString(bl.data)
 	buffer.WriteString(string(bl.previous_hash))
 	buffer.WriteString(fmt.Sprint(bl.timestamp))
+	buffer.WriteString(string(bl.nonce))
 	
 	// create hash and set it to the current block
 	h := sha256.New()
@@ -62,15 +64,26 @@ func create_block(data string, previous_block block) block {
 		timestamp: uint32(time.Now().Unix()), 
 		data: data,
 		previous_hash: previous_block.hash,
+		nonce: 0,
 	}
 	new_block.hash = new_block.get_hash()
 
+	// Proof of Work
+	// Find a hash that has the first 2 values equal to "00"
+	// Should be a race by other nodes to find this node first
+	// Exponential growth - makes it very hard to calculate depending
+	// on the number of 0s (aka the difficulty level)
+	for (string(new_block.hash[0:3]) != "000") {
+		new_block.nonce += 1
+		new_block.hash = new_block.get_hash()
+		fmt.Println(new_block.nonce);
+	}
 	return new_block
 }
 
 func main() {
 	bc := blockchain{}
 	bc.add_block("Hello")
-	bc.add_block("World")
+	//bc.add_block("World")
 	fmt.Println(bc.blocks);
 }
