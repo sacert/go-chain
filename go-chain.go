07150a67695d7b2529003c79b159de6314152985
Stephen Kang <jkkang@sfu.ca>
2017-12-02 22:51:19 -0500

    Basic block chain implementation

--- go-chain.go
+++ go-chain.go
@@ -1,19 +1,14 @@
-/* Disclaimer, first attempt at writing in Go,
- * still trying to understand some of the kinks
- * while learning proper Go syntaxing. 
- * 
- * Hello world
- *
- */
-
 package main
 
 import (
 	"fmt"
+	"bytes"
 	"crypto/sha256"
-	"encoding/hex"
+	"time"
 )
 
+// will be used in the future 
+// keep it simple for now 
 type transaction struct {
 	sender		string
 	reciever	string
@@ -21,40 +16,61 @@ type transaction struct {
 }
 
 type block struct {
-	index		uint
-	timestamp	uint32
-	transactions	transaction
-	proof_of_work	string
-	previous_hash	string
+	index			uint
+	timestamp		uint32
+	data			string
+	previous_hash	[]byte
+	hash 			[]byte
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+// check to see if the block is the genisis block otherwise add the block
+// gensis block is the first block within the chain and will not be able
+// to use the previous block for hashing or contain previous_hash
+func (chain *blockchain) add_block(data string) {
+
+	// check if the genesis block
+	previous_block := block{}
+	if (len(chain.blocks) != 0) {
+		previous_block = chain.blocks[len(chain.blocks)-1]
+	}
+	new_block := create_block(data, previous_block)
+	chain.blocks = append(chain.blocks, new_block)
+}
+
+func (bl *block) get_hash() []byte {
+
+	// Combine the headers 'data', 'previous_hash', and 'timestamp' and hash it	
+	var buffer bytes.Buffer
+	buffer.WriteString(bl.data)
+	buffer.WriteString(string(bl.previous_hash))
+	buffer.WriteString(fmt.Sprint(bl.timestamp))
+	
+	// create hash and set it to the current block
+	h := sha256.New()
+	h.Write(buffer.Bytes())
+	return h.Sum(nil)
+}
+
+func create_block(data string, previous_block block) block {
+	
+	new_block := block {
+		index: previous_block.index + 1, 
+		timestamp: uint32(time.Now().Unix()), 
+		data: data,
+		previous_hash: previous_block.hash,
+	}
+	new_block.hash = new_block.get_hash()
+
+	return new_block
 }
 
 func main() {
-	
-	// testing creation of transaction and block
-	trans := transaction {
-			sender: "abc", 
-			reciever: "xyz", 
-			amount: 5.0
-	}
-	
-	blk := block {
-			index: 1, 
-			timestamp: 232, 
-			transactions: trans, 
-			previous_hash: "hi"
-	}
-	
-	fmt.Println(blk)
-	fmt.Println(trans)
-
-	// testing creation of sha256
-	s := "Welcome to the blockchain"
-    	h := sha256.New()
-    	h.Write([]byte(s))
-    	sha1_hash := hex.EncodeToString(h.Sum(nil))
-	
-	// Hello :)
-	fmt.Println("Hello, world")
-	fmt.Println(sha1_hash)
-
+	bc := blockchain{}
+	bc.add_block("Hello")
+	bc.add_block("World")
+	fmt.Println(bc.blocks);
 }
